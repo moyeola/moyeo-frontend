2e7f704b62444b304bb079b73715ca205a6b4e93
혀느현스 <hyuns@hyuns.dev>
2023-07-31 17:33:07 +0900

    feat: 라이브러리 초기화

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { MainRouter } from "./pages/router";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { RecoilRoot } from "recoil";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient();
 
 function App() {
     return (
-        <div className="App">
-            <MainRouter />
-        </div>
+        <RecoilRoot>
+            <QueryClientProvider client={queryClient}>
+                <MainRouter />
+                <ReactQueryDevtools />
+            </QueryClientProvider>
+        </RecoilRoot>
     );
 }
 
