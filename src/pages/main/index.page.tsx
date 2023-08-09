91bd7cc72cc8d07157c450617fd04a3b81b01ea3
Twince <tjsdn811@gmail.com>
2023-08-10 05:37:43 +0900

    feat: 메인 페이지(.tsx) 추가

---
+++ src/pages/main/index.page.tsx
@@ -0,0 +1,20 @@
+import {
+  Button,
+  Container,
+  Flex,
+  BottomLayout,
+  Image,
+  PageLayout,
+  Text,
+} from "../../components";
+import MoyeoColorLogo from "../../assets/logo/moyeoColorLogo.png";
+
+export function MainPage() {
+  return (
+  <>
+    <PageLayout>
+      
+    </PageLayout>
+  </>
+  );
+}
