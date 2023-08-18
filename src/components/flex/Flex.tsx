6dd6ccb94f43422a8ef41e732c854731373cef26
Twince <tjsdn811@gmail.com>
2023-08-19 06:19:07 +0900

    feat: flex 컴포넌트 프로퍼티(props) 추가

--- src/components/flex/Flex.tsx
+++ src/components/flex/Flex.tsx
@@ -14,14 +14,27 @@ export type FlexProps = {
         | "flex-end"
         | "space-between"
         | "space-around";
+    alignSelf?: 
+        | "center";
+    alignContent?:
+        | "center";
     wrap?: "wrap" | "nowrap";
     gap?: number;
+    width?: string;
+    height?: string;
 };
 export const Flex = styled.div<FlexProps>`
     display: flex;
     flex-direction: ${({ column }) => (column ? "column" : "row")};
+    flex-wrap: ${({ wrap }) => wrap || "nowrap"};
+
     justify-content: ${({ justify }) => justify || "flex-start"};
     align-items: ${({ align }) => align || "flex-start"};
-    flex-wrap: ${({ wrap }) => wrap || "nowrap"};
+    align-self: ${({ alignSelf }) => alignSelf || "center"};
+    align-content: ${({ alignContent }) => alignContent || "center"}
+    
     gap: ${({ gap }) => gap || 0}px;
+
+    width: ${({ width }) => width || "auto"};
+    height: ${({ height }) => height || "auto"};
 `;
