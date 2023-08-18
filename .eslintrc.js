28567bc74dd7a90faefbe5c5fb36c64f8bc85b73
Twince <tjsdn811@gmail.com>
2023-08-19 06:08:31 +0900

    chore: lint 설정 업데이트 및 포맷팅

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,22 +3,19 @@ module.exports = {
         browser: true,
         es2021: true,
     },
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:react/recommended',
-        'next/core-web-vitals',
-        'prettier',
-    ],
+    extends: ['eslint:recommended', 'next/core-web-vitals', 'prettier'],
     plugins: ['import'],
     rules: {
         indent: ['error', 4],
         semi: ['warn', 'always'],
+        'no-unused-vars': 'off',
+        'no-undef': 'off',
+        'no-unescaped-entities': 'off',
         'no-trailing-spaces': 0,
-        'keyword-spacing': 0,
-        'no-unused-vars': 1,
         'no-multiple-empty-lines': 0,
+        'keyword-spacing': 0,
         'space-before-function-paren': 0,
         'eol-last': 0,
+
     },
 };
