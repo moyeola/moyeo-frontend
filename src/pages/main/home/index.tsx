8c3a8af693d35144068d67424cb7304e85875272
Twince <tjsdn811@gmail.com>
2023-08-20 02:20:18 +0900

    refactor: main/home/index 페이지 지정

---
+++ src/pages/main/home/index.tsx
@@ -0,0 +1,18 @@
+import {
+    Button,
+    Container,
+    Flex,
+    BottomLayout,
+    Image,
+    PageLayout,
+    Text,
+} from '../../../components';
+import { Header } from '../home/component/Header';
+
+export function Home() {
+    return (
+        <>
+            <Header />
+        </>
+    );
+}
