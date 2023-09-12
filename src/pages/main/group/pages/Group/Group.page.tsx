360f7645e2944786ae4a95377811c0fd508ef7cb
혀느현스 <hyuns@hyuns.dev>
2023-09-12 20:52:55 +0900

    feat: Group 팀 도구 그룹 초대 기능 추가

--- src/pages/main/group/pages/Group/Group.page.tsx
+++ src/pages/main/group/pages/Group/Group.page.tsx
@@ -3,7 +3,9 @@ import { GroupHeader } from "./containers/GroupHeader";
 import {
     Calendar,
     Flex,
+    IconButton,
     Layout,
+    Modal,
     Section,
     useModal,
 } from "../../../../../libs/ui";
@@ -18,6 +20,8 @@ import { styled } from "styled-components";
 import CalendarIcon from "./assets/calendar.png";
 import FlagIcon from "./assets/flag.png";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import { LinkSimple } from "@phosphor-icons/react";
 
 function GroupCalendarSection() {
     const modal = useModal();
@@ -112,13 +116,15 @@ function GroupToolItem({
     name,
     icon,
     to,
+    onClick,
 }: {
     name: string;
     icon: string;
-    to: string;
+    to?: string;
+    onClick?: () => void;
 }) {
     return (
-        <StyledGroupToolItem to={to}>
+        <StyledGroupToolItem to={to || ""} onClick={() => onClick && onClick()}>
             <StyledGroupToolItemImage src={icon} />
             {name}
         </StyledGroupToolItem>
@@ -133,12 +139,58 @@ function DummyGroupToolItem() {
     );
 }
 
+function InviteGroupModal({ url }: { url: string }) {
+    const modal = useModal();
+
+    const copyLink = () => {
+        navigator.clipboard.writeText(url);
+        toast.info("팀 초대 링크가 클립보드에 복사되었습니다.");
+        modal.close();
+    };
+
+    return (
+        <Modal>
+            <Modal.Header>아래 링크를 공유해 팀을 초대해주세요</Modal.Header>
+            <Modal.Body>
+                <Flex.Row gap="12px">
+                    <Flex width="100%">{url}</Flex>
+                    <IconButton onClick={() => copyLink()}>
+                        <LinkSimple />
+                    </IconButton>
+                </Flex.Row>
+            </Modal.Body>
+        </Modal>
+    );
+}
+
 function GroupToolSection() {
+    const modal = useModal();
+
+    const shareGroup = async () => {
+        if (window.navigator.share === undefined) {
+            modal.open(
+                <InviteGroupModal url="https://moyeo.la/invite/xxxxxx" />
+            );
+
+            return;
+        }
+
+        await window.navigator.share({
+            title: document.title,
+            text: "Hello World",
+            url: "https://developer.mozilla.org",
+        });
+    };
+
     return (
         <Section>
             <Section.Header title="팀 도구" />
             <Flex.Row gap="12px">
-                <GroupToolItem name="팀 초대" icon={FlagIcon} to="" />
+                <GroupToolItem
+                    name="팀 초대"
+                    icon={FlagIcon}
+                    onClick={() => shareGroup()}
+                />
                 <GroupToolItem name="일정 조율" icon={CalendarIcon} to="" />
                 <DummyGroupToolItem />
             </Flex.Row>
