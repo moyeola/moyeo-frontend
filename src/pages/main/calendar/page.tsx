5e123ba6f20b2e451ef5ea86f9da1954f491e1ad
Pyeoun Yuna <78300392+Drizzle03@users.noreply.github.com>
2023-09-15 10:05:08 +0900

    Feat: 일정 필터링

--- src/pages/main/calendar/page.tsx
+++ src/pages/main/calendar/page.tsx
@@ -2,18 +2,25 @@ import { cv } from "../../../libs/ui/style";
 import { Header } from "../../../libs/ui";
 import FilterIcon from "./assets/Filter.png";
 import { AppNavBar } from "../../../containers/AppNavBar/AppNavBar";
+import { useNavigate } from "react-router-dom";
+
 export function CalendarPage() {
+    const navigate = useNavigate(); // Call the hook to get the navigate function
+    
     return (
         <>
             <Header bgColor={cv.bgHome}>
                 <Header.Left></Header.Left>
                 <Header.Title>캘린더</Header.Title>
                 <Header.Right>
-                    <img src={FilterIcon} alt="필터" />
+                    <img 
+                    src={FilterIcon}
+                    alt="필터"
+                    onClick={() => navigate("./filter")}
+                    />
                 </Header.Right>
             </Header>
             <AppNavBar selected="calendar" />
         </>
-        
     );
 }
--- src/pages/main/calendar/router.tsx
+++ src/pages/main/calendar/router.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import { CalendarPage } from "./page";
+import { FilterCalendarPage } from "./pages/FilterCalendar/FilterCalendar.page";
 
 export function CalendarRouter() {
     return (
         <Routes>
             <Route path="/" element={<CalendarPage/>} />
+            <Route path="/filter" element={<FilterCalendarPage/>} />
         </Routes>
     );
 }
