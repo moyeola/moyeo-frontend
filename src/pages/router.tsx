7b0249d2555a64987e4889867c064ac74985686d
혀느현스 <hyuns@hyuns.dev>
2023-09-17 21:30:08 +0900

    feat: terms 페이지 추가

--- src/pages/router.tsx
+++ src/pages/router.tsx
@@ -3,12 +3,14 @@ import { IntroPage } from "./intro/index.page";
 import { AuthRouter } from "./auth/router";
 import { MainRouter } from "./main/router";
 import { Redirect } from "../components";
+import { TermsofService } from "./terms/Terms.page";
 
 export function RootRouter() {
     return (
         <Routes>
             <Route path="/" element={<Redirect to="/main/home" />} />
             <Route path="/intro" element={<IntroPage />} />
+            <Route path="/terms" element={<TermsofService />} />
             <Route path="/auth/*" element={<AuthRouter />} />
             <Route path="/main/*" element={<MainRouter />} />
         </Routes>
